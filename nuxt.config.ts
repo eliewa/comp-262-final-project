50b9fffa26189978c419a7f8f132139e32a80da6
Eli Ewa <elishamaewa@gmail.com>
2022-12-02 21:20:05 -0700

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    // '@nuxtjs/google-fonts'
+  ],
+
+})
+
+// googleFonts: {
+//   families: {
+//     Poppins: true,
+//     Montserrat: true,
+
+//   }
+// }
