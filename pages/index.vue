16e31ddb9e4988199f30292469e7f22db807892a
Eli Ewa <elishamaewa@gmail.com>
2022-12-04 18:57:23 -0700

    added hero footer

--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,19 @@
         </div>
       </div>
     </AppSection>
-    <AppSection>
-      <img src="../assets/images/final-footer.jpg" alt="people" class="relative top-64" >
-    </AppSection>
-   
+
+    <div class="hero-footer pt-10 pb-10">
+      <img src="../assets/images/footer-hero-2.jpg" alt="" />
+      <img src="../assets/images/hero.jpg" alt="" />
+      <img src="../assets/images/footer-hero-4.jpg" alt="" />
+    </div>
+
     <TheFooter />
-    
-    
   </div>
 </template>
 
 <script setup>
-  const { data: products } = await useFetch("https://fakestoreapi.com/products");
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
 </script>
 
 <style>
@@ -28,6 +29,20 @@
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+}
+
+.hero-footer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  position: relative;
+  top: 250px;
+}
+
+.hero-footer img {
+  max-width: 400px;
+  min-width: 200px;
 }
 
 /* .doormat {
