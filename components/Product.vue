0527cb13ffb464175dd923d2f21900db832711a7
Eli Ewa <elishamaewa@gmail.com>
2022-12-06 22:18:51 -0700

    added search bar functionality

---
+++ components/Product.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <article class="p-4 font-semibold font-sans">
+      <p class="font-heading">{{title}}</p>
+      <img :src="image" alt="" class="h-40 w-28 mx-auto p-2 object-cover">
+      <p class="p-2 text-center">$ {{price}}</p>
+    </article>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: {
+      type: String,
+      default: 'Air Force 1',
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: String,
+      default: '129.99'
+    }
+  })
+</script>
--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <header class="fixed font-heading bg-black text-white text-2xl p-3 text-center w-[100%]">
+    <header
+      class="fixed font-heading bg-black text-white text-2xl p-3 text-center w-[100%]"
+    >
       <p>FREE SHIPPING WORLDWIDE</p>
     </header>
-    <nav class="fixed flex justify-around font-heading top-14 p-4 text-lg bg-white w-[100%]">
+    <nav
+      class="fixed flex justify-around font-heading top-14 p-4 text-lg bg-white w-[100%]"
+    >
       <div class="flex md:justify-left gap-10 items-center">
         <div class="text-3xl">
           <button @click.prevent="menuToggle">
@@ -30,26 +34,47 @@
         <TheLogo />
         <div class="nav-links">
           <ul class="hidden lg:flex gap-10">
-          <li>
-            <NuxtLink to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/products">Shop</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about">About</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/blog">Blog</NuxtLink>
-          </li>
-        </ul>
+            <li>
+              <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/products">Shop</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about">About</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/blog">Blog</NuxtLink>
+            </li>
+          </ul>
         </div>
-        
       </div>
 
       <div class="flex gap-4 items-center">
-        <input type="text" name="search" id="search" placeholder="Search..." class="border-black border-2 p-1 w-1/2 invisible md:visible rounded-md">
-        <Icon name=fa6-solid:magnifying-glass class="text-2xl"/>
+        <div>
+          <input
+            type="text"
+            v-model="search"
+            placeholder="Search..."
+            class="border-black border-2 p-1 w-1/2 invisible md:visible rounded-md"
+          />
+          <button @click.prevent="productToggle">
+            <Icon
+              name="fa6-solid:magnifying-glass"
+              class="text-2xl hover:border-2 border-black ml-4"
+            />
+          </button>
+          <div v-show="showProduct">
+            <div class="bg-white w-56 h-96 border-2 absolute overflow-y-scroll">
+              <div v-for="p in matchingProducts" :key="p.id">
+                <NuxtLink :to="link + p.id">
+                  <Product :title="p.title" :image="p.image" :price="p.price" />
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <p>Login</p>
         <Icon name="fa6-solid:user-large" />
       </div>
@@ -60,16 +85,31 @@
 <script setup>
 const showMenu = ref(false);
 const menuToggle = () => (showMenu.value = !showMenu.value);
+
+const showProduct = ref(false);
+const productToggle = () => (showProduct.value = !showProduct.value);
+
+const search = ref("");
+
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+const link = "/products/";
+
+const matchingProducts = computed(() => {
+  return products.value.filter((product) =>
+    product.title.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
 </script>
 
 <style>
-  nav, header {
-    z-index: 20;
-  }
-  
-  nav .nav-links ul li::after{
+nav,
+header {
+  z-index: 20;
+}
+
+nav .nav-links ul li::after {
   /* hover styling */
-  content: '';
+  content: "";
   width: 0;
   height: 2px;
   background: black;
@@ -78,14 +118,14 @@ const menuToggle = () => (showMenu.value = !showMenu.value);
   transition: 0.5s;
 }
 
-nav .nav-links ul li:hover::after{
+nav .nav-links ul li:hover::after {
   /* hover styling */
   width: 100%;
 }
 
-nav .nav-links-white ul li::after{
+nav .nav-links-white ul li::after {
   /* hover styling */
-  content: '';
+  content: "";
   width: 0;
   height: 2px;
   background: white;
@@ -94,7 +134,7 @@ nav .nav-links-white ul li::after{
   transition: 0.5s;
 }
 
-nav .nav-links-white ul li:hover::after{
+nav .nav-links-white ul li:hover::after {
   /* hover styling */
   width: 15%;
 }
